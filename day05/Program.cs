7b5b4b085a39252f7d015bbef7ab4e4ff36c9539
Jens Larsson <Larssonjenz@gmail.com>
2024-12-09 01:28:02 +0100

    Refactor and add solution for part 2 of day 5

--- day05/Program.cs
+++ day05/Program.cs
@@ -1,46 +1,75 @@
-﻿public class Node
+﻿public class Node(int value)
 {
-    public int Value;
-    public Node? Larger;
-    public Node? Smaller;
+    public int Value { get; init; } = value;
+    public Dictionary<int, Node> Larger = new();
+    public Dictionary<int, Node> Smaller = new();
 
-    public Node(int value, int larger)
+    public void AddLarger(Node node)
     {
-        Value = value;
-        Larger = new Node(larger);
+        Larger.TryAdd(node.Value, node);
     }
-    public Node(int value)
+    public void AddSmaller(Node node)
     {
-        Value = value;
+        Smaller.TryAdd(node.Value, node);
+    }
+
+    public Node GetLarger(int value)
+    {
+        // if (!Larger.TryGetValue(value, out var larger))
+        // {
+        //     foreach (var n in Larger)
+        //     {
+        //         larger = n.Value.GetLarger(value);
+        //         if (larger != null)
+        //         {
+        //             return larger;
+        //         }
+        //     }
+        // }
+        Larger.TryGetValue(value, out var larger);
+        return larger;
+    }
+
+    public Node GetSmaller(int value)
+    {
+        // if (!Smaller.TryGetValue(value, out var smaller))
+        // {
+        //     foreach (var n in Smaller)
+        //     {
+        //         smaller = n.Value.GetSmaller(value);
+        //         if (smaller != null)
+        //         {
+        //             return smaller;
+        //         }
+        //     }
+        // }
+        Smaller.TryGetValue(value, out var smaller);
+        return smaller;
     }
 }
-
 class Program
 {
     static void Main()
     {
         var lines = File.ReadLines("input.txt");
-        var nodes = new Dictionary<int, List<int>>();
+        var nodes = new Dictionary<int, Node>();
         var numbers = new List<List<int>>();
-
-
 
         foreach (string line in lines)
         {
             var parts = line.Split('|', StringSplitOptions.None);
             if (parts.Count() == 2)
             {
-                int smaller = int.Parse(parts[0]);
-                int larger = int.Parse(parts[1]);
-                if (nodes.TryGetValue(smaller, out var list))
-                {
-                    list.Add(larger);
-                }
-                else
-                {
-                    nodes.Add(smaller, new List<int> { larger });
-                }
-                nodes.TryAdd(larger, new List<int>());
+                int s = int.Parse(parts[0]);
+                int l = int.Parse(parts[1]);
+                nodes.TryGetValue(s, out var small);
+                nodes.TryGetValue(l, out var large);
+                small ??= new Node(s);
+                large ??= new Node(l);
+                small.AddLarger(large);
+                large.AddSmaller(small);
+                nodes.TryAdd(s, small);
+                nodes.TryAdd(l, large);
                 continue;
             }
             parts = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
@@ -56,30 +85,52 @@ class Program
         }
 
         int combined = 0;
+        HashSet<List<int>> incorrectLists = new();
 
         foreach (var list in numbers)
         {
-            nodes.TryGetValue(list[0], out var larger);
-            bool completed = true;
-            for (int i = 1; i < list.Count(); i++)
+            if (IsOrdered(list, nodes[list[0]]) == list.Count())
             {
-                if (larger.Contains(list[i]))
-                {
-                    larger = nodes[list[i]];
-                    continue;
-                }
-                else
-                {
-                    completed = false;
-                    break;
-                }
+                combined += list[list.Count() / 2];
             }
-            if (completed)
+            else
             {
-                combined += list[list.Count / 2];
+                incorrectLists.Add(list);
             }
         }
         Console.WriteLine(combined);
+        Console.WriteLine(incorrectLists.Count());
+        combined = 0;
+        foreach (var list in incorrectLists)
+        {
+            int i = 0;
+            do
+            {
+                i = IsOrdered(list, nodes[list[0]]);
+                if (i != list.Count())
+                {
+                    int temp = list[i];
+                    list[i] = list[i - 1];
+                    list[i - 1] = temp;
+                }
+            } while (i != list.Count());
+            combined += list[list.Count() / 2];
+        }
+        Console.WriteLine(combined);
+    }
 
+    static int IsOrdered(List<int> numbers, Node ruleRoot)
+    {
+        Node larger = ruleRoot;
+        int i;
+        for (i = 1; i < numbers.Count; i++)
+        {
+            larger = larger.GetLarger(numbers[i]);
+            if (larger == null)
+            {
+                break;
+            }
+        }
+        return i;
     }
 }
